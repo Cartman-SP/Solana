68498f38b13f431a464072343e8af9588693919a
F1ower <37472980+Cartman-SP@users.noreply.github.com>
2025-08-12 04:09:38 +0300

    123

--- backend/mainapp/admin.py
+++ backend/mainapp/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import AdminDev, UserDev, Token
 
+class AdminTwitterFilter(admin.SimpleListFilter):
+    title = 'Twitter админа'
+    parameter_name = 'admin_twitter'
+    
+    def lookups(self, request, model_admin):
+        # Получаем уникальные Twitter админов
+        admins = AdminDev.objects.exclude(twitter='').values_list('twitter', 'twitter').distinct()
+        return admins
+    
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(admin__twitter=self.value())
+        return queryset
+
 @admin.register(AdminDev)
 class AdminDevAdmin(admin.ModelAdmin):
     list_display = ('twitter', 'blacklist', 'whitelist', 'ath')
@@ -11,7 +25,7 @@ class AdminDevAdmin(admin.ModelAdmin):
 @admin.register(UserDev)
 class UserDevAdmin(admin.ModelAdmin):
     list_display = ('adress', 'admin', 'total_tokens', 'whitelist', 'blacklist', 'ath', 'processed')
-    list_filter = ('whitelist', 'blacklist', 'processed')
+    list_filter = ('whitelist', 'blacklist', 'processed', AdminTwitterFilter)
     search_fields = ('adress', 'uri')
     ordering = ('-total_tokens', 'adress')
     list_per_page = 50
