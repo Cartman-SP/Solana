b72b57037661cf3f7b04084ca1ca50db22e61477
F1ower <37472980+Cartman-SP@users.noreply.github.com>
2025-08-13 13:27:03 +0300

    123

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -20,6 +20,8 @@ class UserDev(models.Model):
     uri = models.CharField(max_length=255, null=True, blank=True)
     processed = models.BooleanField(default=False)
     faunded = models.BooleanField(default=False)
+    def __str__(self):
+        return self.adress
 
 class Token(models.Model):
     address = models.CharField(max_length=255)
