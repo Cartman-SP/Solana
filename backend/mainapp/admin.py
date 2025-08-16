a03e4995b2df772281102d785e1c10c24cf58b92
F1ower <37472980+Cartman-SP@users.noreply.github.com>
2025-08-16 22:42:21 +0300

    123

--- backend/mainapp/admin.py
+++ backend/mainapp/admin.py
@@ -1,23 +1,6 @@
 from django.contrib import admin
 from .models import AdminDev, UserDev, Token
 
-class HasAdminFilter(admin.SimpleListFilter):
-    title = 'Наличие админа'
-    parameter_name = 'has_admin'
-    
-    def lookups(self, request, model_admin):
-        return (
-            ('yes', 'Да'),
-            ('no', 'Нет'),
-        )
-    
-    def queryset(self, request, queryset):
-        if self.value() == 'yes':
-            return queryset.filter(admin__isnull=False)
-        elif self.value() == 'no':
-            return queryset.filter(admin__isnull=True)
-        return queryset
-
 class TotalTokensFilter(admin.SimpleListFilter):
     title = 'Количество токенов'
     parameter_name = 'total_tokens'
@@ -37,23 +20,23 @@ class TotalTokensFilter(admin.SimpleListFilter):
 
 @admin.register(AdminDev)
 class AdminDevAdmin(admin.ModelAdmin):
-    list_display = ('twitter', 'blacklist', 'whitelist', 'ath',"total_devs","total_tokens")
+    list_display = ('twitter', 'blacklist', 'whitelist', 'ath', 'total_devs', 'total_tokens')
     list_filter = ('blacklist', 'whitelist')
     search_fields = ('twitter',)
-    ordering = ('twitter','total_devs')
+    ordering = ('twitter', 'total_devs')
 
 @admin.register(UserDev)
 class UserDevAdmin(admin.ModelAdmin):
-    list_display = ('id','adress', 'admin' , 'whitelist', 'blacklist', 'ath', 'processed','total_tokens','faunded_by',"faunded")
-    list_filter = ('whitelist', 'blacklist', 'processed', HasAdminFilter, TotalTokensFilter)
-    search_fields = ('adress', 'uri', 'admin__twitter')
+    list_display = ('id', 'adress', 'total_tokens')
+    list_filter = (TotalTokensFilter,)
+    search_fields = ('adress',)
     ordering = ('-total_tokens', 'adress')
     list_per_page = 50
 
 @admin.register(Token)
 class TokenAdmin(admin.ModelAdmin):
-    list_display = ('address', 'dev', 'scam', 'ath', 'migrated', 'created_at','processed')
-    list_filter = ('scam', 'migrated', 'created_at','processed')
+    list_display = ('address', 'dev', 'twitter', 'ath', 'migrated', 'created_at', 'processed')
+    list_filter = ('migrated', 'created_at', 'processed')
     search_fields = ('address', 'dev__adress')
     ordering = ('-created_at', 'address')
     list_per_page = 50
