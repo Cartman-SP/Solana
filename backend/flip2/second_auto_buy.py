4f29b231ca11fef40b9af6dcbb17231e26eb37bf
F1ower <37472980+Cartman-SP@users.noreply.github.com>
2025-08-18 13:09:58 +0300

    123

--- backend/flip2/second_auto_buy.py
+++ backend/flip2/second_auto_buy.py
@@ -123,9 +123,9 @@ async def process_message(msg, session):
         logs = msg.get("params", {}).get("result", {}).get("value", {}).get("logs", [])
         if not any(INSTRUCTION_CREATE_RE.search(log) for log in logs):
             return
-        print(logs)
+        print(123)
         with open('test555.txt', 'a') as f:
-            f.write(str(logs))
+            f.write(str(msg))
 
         data = collect_progdata_bytes_after_create(logs)
         parsed = parse_pump_create(data)
