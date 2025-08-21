3fba6cb1ad40694c18b06e2cbae3349bf88a3bda
F1ower <37472980+Cartman-SP@users.noreply.github.com>
2025-08-21 18:43:07 +0300

    12312

--- backend/flip/autobuy.py
+++ backend/flip/autobuy.py
@@ -414,8 +414,8 @@ async def process_message(msg, session):
         priorityFee = float(settings_obj.priority_fee_sol)
 
         # Запускаем обе операции как awaitable
-        create_invoice_task = asyncio.create_task(generate_tx, pubkey, mint, amount, slippage, priorityFee)
-        checker_task = asyncio.create_task(checker(session, uri, creator))
+        create_invoice_task = generate_tx, pubkey, mint, amount, slippage, priorityFee
+        checker_task = checker(session, uri, creator)
 
         # Ждем завершения обеих задач
         results = await asyncio.gather(
