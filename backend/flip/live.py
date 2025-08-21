e825ff7faa23a3944a735619fb81045ca6af043b
F1ower <37472980+Cartman-SP@users.noreply.github.com>
2025-08-21 18:45:33 +0300

    123

--- backend/flip/live.py
+++ backend/flip/live.py
@@ -239,14 +239,10 @@ async def process_live(data):
         print(symbol)
         if twitter == '':
             return
-        autobuy_task = check_twitter_whitelist(twitter, user)
-        user_dev_data_task = get_user_dev_data(user,mint)
-        twitter_data_task = get_twitter_data(twitter,mint)
-        print(123)
         results = await asyncio.gather(
-            autobuy_task,
-            user_dev_data_task,
-            twitter_data_task,
+            check_twitter_whitelist(twitter, user),
+            get_user_dev_data(user, mint),
+            get_twitter_data(twitter, mint),
         )
         print(results)
         autobuy, user_dev_data, twitter_data = results
