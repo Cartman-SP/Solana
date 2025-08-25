74ce421cec2da0687584a838a130c397d25bd52c
F1ower <37472980+Cartman-SP@users.noreply.github.com>
2025-08-26 01:39:29 +0300

    123

--- backend/mainapp/admin.py
+++ backend/mainapp/admin.py
@@ -45,7 +45,7 @@ class TokenAdmin(admin.ModelAdmin):
 
 @admin.register(Settings)
 class SettingsAdmin(admin.ModelAdmin):
-    list_display = ('buyer_pubkey', 'sol_amount', 'slippage_percent', 'priority_fee_sol',  'start','median')
+    list_display = ('buyer_pubkey', 'sol_amount', 'slippage_percent', 'priority_fee_sol',  'start','median','total_fees_from')
     list_filter = ('start',)
     search_fields = ('buyer_pubkey',)
     ordering = ('-sol_amount',)
